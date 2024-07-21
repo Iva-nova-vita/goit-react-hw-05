df6952d6bc5d1012bee8c24945e5bcb12a4a5294
v.ivanova <viktoriia.ivanova@vista.com>
2024-07-21 18:08:26 +0300

    default image

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,18 +1,31 @@
 import css from './MovieInfo.module.css';
 export default function MovieInfo({ info }) {
   const { poster_path, title, vote_average, overview, genres } = info;
+  const defaultImg =
+    'https://dummyimage.com/400x600/cdcdcd/000.jpg&text=No+poster';
   console.log(info);
   return (
     <div className={css.card}>
       <img
-      width='300'
-      height='450'
+        width='300'
+        height='450'
         className={css.cardImg}
         src={'https://image.tmdb.org/t/p/w500' + poster_path}
       ></img>
+      <img
+        src={
+          poster_path
+            ? 'https://image.tmdb.org/t/p/w500' + poster_path
+            : defaultImg
+        }
+        width={250}
+        alt='poster'
+      />
       <div className={css.cardInfo}>
         <div className={css.cardTitle}>{title}</div>
-        <div className={css.cardAverage}><span>Rating:</span> {vote_average}</div>
+        <div className={css.cardAverage}>
+          <span>Rating:</span> {vote_average}
+        </div>
         <div className={css.bold}>Overview</div>
         <div className={css.cardOverview}>{overview}</div>
         <div className={css.bold}>Genres:</div>
